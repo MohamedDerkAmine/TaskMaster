77cb3972461cee052df1a7a2593299349306099e
MohamedDerkAmine <mohamedderk512003@gmail.com>
2023-05-26 21:07:36 +0100

    remove title Task Master

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,19 +1,13 @@
-import Head from "next/head";
 import { TopBar, Board, SideBar } from "~/components";
 
 export default function Main() {
   return (
-    <>
-      <Head>
-        <title>Task Master</title>
-      </Head>
-      <div className="flex h-screen">
-        <SideBar />
-        <div className="flex w-full flex-col lg:w-4/5">
-          <TopBar />
-          <Board />
-        </div>
+    <div className="flex h-screen">
+      <SideBar />
+      <div className="flex w-full flex-col lg:w-4/5">
+        <TopBar />
+        <Board />
       </div>
-    </>
+    </div>
   );
 }
