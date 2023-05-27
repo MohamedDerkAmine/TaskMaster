7e3a45c554cbb06ef3dc8be69d8cc810405935ea
MohamedDerkAmine <mohamedderk512003@gmail.com>
2023-05-27 13:48:37 +0100

    remove dahsboard related modules from components and its barrel file

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,3 @@
-export { default as SideBar } from "./Sidebar";
-export { default as TopBar } from "./Topbar";
-export { default as Board } from "./Board";
 export { default as Input } from "./Input";
 export { default as Dropdown } from "./Dropdown";
 export { default as Col } from "./Column";
